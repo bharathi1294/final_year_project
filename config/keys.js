564071cc84d844f3ce8f75436f3b9683a9167d96
Bharathi_S <sivabharathi1294@gmail.com>
2021-02-04 15:13:59 +0530

    =Initial

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = "mongodb://localhost:27017/login_passport";
+
+module.exports = {
+    mongoURI: dbPassword
+};
---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+
+// Welcome Page
+router.get('/', forwardAuthenticated, (req, res) => res.render('login_temp/welcome'));
+
+// Dashboard
+router.get('/dashboard', ensureAuthenticated, (req, res)=>{
+  res.render('dash_temp/dashboard', {
+    user: req.user
+  })
+});
+module.exports = router;
