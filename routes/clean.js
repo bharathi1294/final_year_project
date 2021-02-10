9d2f4ad0cd0c6a8f95a44c42328a249e17457919
Bharathi_S <sivabharathi1294@gmail.com>
2021-02-10 12:08:41 +0530

    Markup

--- routes/clean.js
+++ routes/clean.js
@@ -5,6 +5,7 @@ const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 
 router.post('/clean',ensureAuthenticated,(req,res)=>{
     console.log(req.body.filename)
+    res.render('dash_temp/analysis',{filename:req.body.filename})
 })
 
 
